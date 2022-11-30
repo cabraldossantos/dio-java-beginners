1270710e059757ab5c903598ce2b024d25eccaf8
Cabral dos Santos <52243232+cabraldossantos@users.noreply.github.com>
2022-11-29 21:54:27 -0300

    Update Carro.java
    Inclusão dos métodos getters e setters, e construtores (com e sem parâmetros).

--- oo-aula-03-1/src/one/digitalinnovation/oo/Carro.java
+++ oo-aula-03-1/src/one/digitalinnovation/oo/Carro.java
@@ -4,4 +4,40 @@ public class Carro {
     String cor;
     String modelo;
     double capacidadeTanque;
+
+    Carro() {}
+
+    Carro(String cor, String modelo, double capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCapacidadeTanque(double capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque = valorCombustivel;
+    }
 }
